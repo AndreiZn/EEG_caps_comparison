ab091c6de996e03866fcce7bb8986b16a7d1afba
Mohammad Ali <ma_sadri2004@yahoo.com>
2019-08-19 17:45:05 +0300

    add description

--- functions/load_files.m
+++ functions/load_files.m
@@ -1,15 +1,18 @@
 function y = load_files()
 files = get_subj_cap_struct();
 num_subj = length(files);
+%% iterate over subjects
 for i=1:num_subj
     subj = files(i).Subject;
     devices = files(i).Devices;
     root_addr = files(i).Root_Addr;
     fprintf('Current subject name is: %s \n',subj);
+    %% iterate over devices
     for j=1:length(devices)
         curr_dev = devices(j);
         fprintf('\t\tCurrent device is: %s\n', curr_dev);
         curr_folder = fullfile(root_addr,curr_dev);
+        %% select device type
         if curr_dev == 'gNautilus'
             eeg_file = dir(fullfile(curr_folder,'Exp*.edf'));
             for k=1:length(eeg_file)
