89a222b6c8c956627182b7676663b9f9d64f69bd
AndreiZn <anznobishchev@gmail.com>
2019-08-20 13:15:06 +0300

    Exp3_analysis and plot function

---
+++ experiment_analysis_functions/Exp3_Analysis.m
@@ -0,0 +1,29 @@
+% Analysis of Experiment 3 (a subject was instructed to press the left mouse button whenever 
+% a red circle appeared on the screen)
+function [EEG] = Exp3_Analysis(y, srate, channel_location_file, time_step_to_plot)
+
+assignin('base','y',y)
+assignin('base','srate',srate)
+
+EEG.etc.eeglabvers = '14.1.2'; % this tracks which version of EEGLAB is being used, you may ignore it
+EEG = pop_importdata('dataformat','array','nbchan',9,'data','y','srate',srate,'pnts',0,'xmin',0);
+EEG = eeg_checkset( EEG );
+EEG = pop_select( EEG,'channel',2:9);
+EEG = eeg_checkset( EEG );
+EEG = pop_editset(EEG, 'chanlocs', channel_location_file);
+EEG = eeg_checkset( EEG );
+eegplot('noui',EEG.data, 'winlength', size(y,2)/srate, 'eloc_file', channel_location_file);
+fig = gcf;
+chldrn = fig.Children; 
+ax = chldrn(3);
+xticks(ax, 1:srate*time_step_to_plot:size(y,2));
+saveas(fig,'Exp3_Plot.png')
+close(gcf)
+
+
+% 
+% %[spectopo_outputs] = pop_spectopo( EEG, dataflag, timerange, process, 'key', 'val',...); % returns spectopo() outputs
+% 
+% %output_set_name = [set_name, '_', output_suffix, '.set'];
+% %EEG = pop_saveset( EEG, 'filename',output_set_name,'filepath',output_folder_cur);
+% %EEG = eeg_checkset( EEG );
--- functions/load_files.m
+++ functions/load_files.m
@@ -1,6 +1,7 @@
 function y = load_files()
 files = get_subj_cap_struct();
 num_subj = length(files);
+time_step_to_plot = 5;
 %% iterate over subjects
 for i=1:num_subj
     subj = files(i).Subject;
@@ -14,14 +15,22 @@ for i=1:num_subj
         curr_folder = fullfile(root_addr,curr_dev);
         %% select device type
         if curr_dev == 'gNautilus'
+            sampling_rate = 250;
+            time_period_to_remove = 20;
+            channel_location_file = 'C:\Users\user.T440_IT\EEG_caps_comparison_main\EEG_caps_comparison_code\eeglab14_1_2b\sample_locs\gGAMMAcap8ch_10-20.locs';
             eeg_file = dir(fullfile(curr_folder,'Exp*.mat'));
             for k=1:length(eeg_file)
                 fprintf('\t\t\t\tCurrent file is: %s\n', eeg_file(k).name);
                 curr_file = fullfile(curr_folder,eeg_file(k).name);
                 y = load(curr_file);
                 y = y.y;
+                y = y(:,sampling_rate*time_period_to_remove:end);
                 %% work to do with y
-                
+                filename_split = split(eeg_file(k).name, '_');
+                exp_num = filename_split{1};
+                if strcmp(exp_num, 'Exp3')
+                    [EEG] = Exp3_Analysis(y, sampling_rate, channel_location_file, time_step_to_plot);
+                end
             end
         elseif curr_dev == 'Neuroplay'
             eeg_file = dir(fullfile(curr_folder,'Exp*.edf'));
