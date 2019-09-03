1ad59a6b1a287fc3fb266a86c8823ea1ee8c787c
AndreiZn <anznobishchev@gmail.com>
2019-09-03 18:54:01 +0300

    minor changes

--- experiment_analysis_functions/Exp1_Analysis.m
+++ experiment_analysis_functions/Exp1_Analysis.m
@@ -1,4 +1,5 @@
-% Analysis of Experiment 1 (a subject was instructed to close his eyes for ~3 minutes and sit still
+% Analysis of Experiment 1 (a subject was instructed to close his eyes for
+% ~3 minutes and sit still)
 function Exp1_Analysis(y,conf)
 
 EEG = exp_analysis_core(y,conf);
--- experiment_analysis_functions/Exp4_Analysis.m
+++ experiment_analysis_functions/Exp4_Analysis.m
@@ -1,6 +1,6 @@
 % Analysis of Experiment 4 (a subject was instructed to control two blue
-% circles (one with the keyboard and the other one with the computer mouse)
-% and avoid red circles moving randomly on the screen
+% circles (one with the keyboard and the other one with the computer mouse
+% and avoid red circles moving randomly on the screen)
 
 function Exp4_Analysis(y,conf)
 
